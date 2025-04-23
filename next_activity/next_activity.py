a2229eb8cc871c0c1b7a444a6141cf0a67e974a7
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-04-23 09:51:30 +0200

    test

--- next_activity/next_activity.py
+++ next_activity/next_activity.py
@@ -31,6 +31,7 @@ class Config:
 
     log = pm4py.read_xes("../data/receipt.xes")
     process_traces_str = pm4py.project_on_event_attribute(log, "concept:name")
+    process_traces_str = list(set(process_traces_str))
     #print(process_traces_str)
     #input()
 
